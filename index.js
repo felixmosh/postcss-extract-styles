59f99a18fc976603385fcdf80bacda3f3730d525
Felix Mosheev <felixm@wix.com>
2016-08-16 15:17:46 +0300

    First step

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-extract-styles', function (opts) {
-    opts = opts || {};
+function extend(a, b) {
+    for (var key in b)
+        if (b.hasOwnProperty(key))
+            a[key] = b[key];
+    return a;
+}
 
-    // Work with options here
+module.exports = postcss.plugin('postcss-extract-styles', function (opts) {
+    opts = extend({
+        remove: false,
+        pattern: false
+    }, opts);
+
+    function handleDeclarations(rule, newRule, options) {
+        rule.walkDecls(function (decl) {
+            if (options.pattern.test(decl.toString())) {
+                var newDecl = decl.clone();
+                newDecl.raws = decl.raws;
+                newRule.append(newDecl);
+
+                if (options.remove) {
+                    decl.remove();
+
+                    if (rule.nodes.length === 0) {
+                        rule.remove();
+                    }
+                }
+            }
+        });
+    }
+
+    function cloneRule(rule) {
+        var newRule = rule.clone();
+
+        newRule.removeAll();
+
+        return newRule;
+    }
+
+    function cloneAtRole(rule) {
+        var newAtRule = rule.clone();
+
+        newAtRule.removeAll();
+
+        return newAtRule;
+    }
+
+    function extractStyles(css, newcss, opts) {
+        css.each(function (rule) {
+            if (rule.type === 'atrule' && rule.walkRules) {
+                var newAtRule = cloneAtRole(rule);
+                extractStyles(rule, newAtRule, opts);
+
+                if (newAtRule.nodes.length) {
+                    newcss.append(newAtRule);
+                }
+                if (opts.remove && rule.nodes.length === 0) {
+                    rule.remove();
+                }
+            }
+            else if (rule.type === 'rule' && rule.walkDecls) {
+                var newRule = cloneRule(rule);
+
+                handleDeclarations(rule, newRule, opts);
+                if (newRule.nodes.length) {
+                    newcss.append(newRule);
+                }
+            }
+        });
+
+    }
+
 
     return function (css, result) {
+        var newcss = postcss.root();
 
-        // Transform CSS AST here
+        extractStyles(css, newcss, opts);
 
+        result.root = opts.remove ? css : newcss;
     };
 });
--- test/test.js
+++ test/test.js
@@ -1,11 +1,23 @@
 var postcss = require('postcss');
-var expect  = require('chai').expect;
+var expect = require('chai').expect;
+var fs = require('fs');
+var path = require('path');
 
 var plugin = require('../');
 
-var test = function (input, output, opts, done) {
-    postcss([ plugin(opts) ]).process(input).then(function (result) {
-        expect(result.css).to.eql(output);
+function readFile(relPath) {
+    return fs.readFileSync(path.join(__dirname, relPath), {encoding: 'utf8'});
+}
+
+var pattern = /\[\[[^\]]+\]\]/;
+
+var test = function (inputFile, opts, done) {
+    var input = readFile(path.join('fixtures', inputFile + '.css'));
+    var expected = readFile(path.join('expected', inputFile + '.css'));
+
+    postcss([plugin(opts)]).process(input).then(function (result) {
+        fs.writeFile(path.join(__dirname, 'temp.css'), result.css);
+        expect(result.css).to.eql(expected);
         expect(result.warnings()).to.be.empty;
         done();
     }).catch(function (error) {
@@ -14,11 +26,7 @@ var test = function (input, output, opts, done) {
 };
 
 describe('postcss-extract-styles', function () {
-
-    /* Write tests here
-
-    it('does something', function (done) {
-        test('a{ }', 'a{ }', { }, done);
-    });*/
-
+    it('extract styles that matches to pattern', function (done) {
+        test('with_remove', {pattern: pattern, remove: true}, done);
+    });
 });
