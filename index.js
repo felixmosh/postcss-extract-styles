ba8d08eb25bb2a9faa7b7c2fb25b0fd7ba37aecc
Felix Mosheev <felixm@wix.com>
2018-02-24 11:59:49 +0200

    small refactor

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
 const postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-extract-styles', (opts) => {
+  return (css, result) => {
+    const extractedCSS = postcss.root();
 
-	function handleDeclarations(rule, newRule, options) {
-		rule.walkDecls((decl) => {
-			if (options.pattern.test(decl.toString())) {
-				const newDecl = decl.clone();
-				newDecl.raws = decl.raws;
-				newRule.append(newDecl);
+    extractStyles(css, extractedCSS, opts);
 
-				decl.remove();
-
-				if (rule.nodes.length === 0) {
-					rule.remove();
-				}
-
-			}
-		});
-	}
-
-	function cloneRule(rule) {
-		const newRule = rule.clone();
-
-		newRule.removeAll();
-
-		return newRule;
-	}
-
-	function extractStyles(css, newcss, opts) {
-		css.each((rule) => {
-			if (rule.type === 'atrule' && rule.walkRules && rule.nodes) {
-				const newAtRule = cloneRule(rule);
-				extractStyles(rule, newAtRule, opts);
-
-				if (newAtRule.nodes.length) {
-					newcss.append(newAtRule);
-				}
-				if (rule.nodes.length === 0) {
-					rule.remove();
-				}
-			}
-			else if (rule.type === 'rule' && rule.walkDecls) {
-				const newRule = cloneRule(rule);
-
-				handleDeclarations(rule, newRule, opts);
-				if (newRule.nodes.length) {
-					newcss.append(newRule);
-				}
-			}
-		});
-	}
-
-	return (css, result) => {
-		const extractedCSS = postcss.root();
-
-		extractStyles(css, extractedCSS, opts);
-
-		result.root = css;
-		result.extracted = extractedCSS.toString();
-	};
+    result.root = css;
+    result.extracted = extractedCSS.toString();
+  };
 });
+
+function extractStyles(css, newcss, opts) {
+  css.each((rule) => {
+    if (rule.type === 'atrule' && rule.walkRules && rule.nodes) {
+      const newAtRule = cloneRule(rule);
+
+      extractStyles(rule, newAtRule, opts);
+      appendIfNotEmpty(newAtRule, newcss);
+      removeIfEmpty(rule);
+    }
+    else if (rule.type === 'rule' && rule.walkDecls) {
+      const newRule = cloneRule(rule);
+
+      extractFromDeclarations(rule, newRule, opts);
+      appendIfNotEmpty(newRule, newcss);
+    }
+  });
+}
+
+function extractFromDeclarations(rule, newRule, options) {
+  rule.walkDecls((decl) => {
+    if (options.pattern.test(decl.toString())) {
+      const newDecl = decl.clone();
+      newDecl.raws = decl.raws;
+      newRule.append(newDecl);
+
+      decl.remove();
+      removeIfEmpty(rule);
+    }
+  });
+}
+
+function cloneRule(rule) {
+  const newRule = rule.clone();
+
+  newRule.removeAll();
+
+  return newRule;
+}
+
+function appendIfNotEmpty(rule, newcss) {
+  if (rule.nodes.length) {
+    newcss.append(rule);
+  }
+}
+
+function removeIfEmpty(rule) {
+  if (rule.nodes.length === 0) {
+    rule.remove();
+  }
+}
